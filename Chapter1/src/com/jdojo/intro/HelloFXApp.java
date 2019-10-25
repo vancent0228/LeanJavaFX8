d879925d814c7d3f69786a8c66909a2e34ae6813
vancent <vancent0228@gmail.com>
2019-10-25 17:05:13 +0800

    company

--- Chapter1/src/com/jdojo/intro/HelloFXApp.java
+++ Chapter1/src/com/jdojo/intro/HelloFXApp.java
@@ -1,7 +1,11 @@
 package com.jdojo.intro;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 public class HelloFXApp extends Application {
 
@@ -11,6 +15,13 @@ public class HelloFXApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Text msg=new Text("Hello JavaFX");
+        VBox root = new VBox();
+        root.getChildren().add(msg);
+
+        Scene scene = new Scene(root, 300, 50);
+        primaryStage.setScene(scene);
+
         // Set a title for the stage
         primaryStage.setTitle("Hello JavaFX Application");
 
